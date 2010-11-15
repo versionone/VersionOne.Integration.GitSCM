02a68686e94c013f549a2d2f2dcdf40d92a191c9
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-11-15 19:05:55 +0000

    a draft of persistence. TODO make it decent

--- src/com/versionone/git/DbStorage.java
+++ src/com/versionone/git/DbStorage.java
@@ -1,19 +1,44 @@
 package com.versionone.git;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import java.sql.SQLException;
 import java.util.Collection;
 
 public class DbStorage implements IDbStorage {
+    private final Session session;
+
+    private Session getSession() {
+        return session;
+    }
+
+    public DbStorage() throws SQLException {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        session = factory.openSession();
+    }
+
     public Collection<PersistentChange> getPersistedChanges() {
-        // TODO
-        return null;
+        return getSession().createCriteria(PersistentChange.class).list(); 
     }
 
     public void persistChange(PersistentChange change) {
-        // TODO
+        Transaction tr = getSession().beginTransaction();
+        getSession().save(change);
+        tr.commit();
+        getSession().flush();
     }
 
     public boolean isChangePersisted(PersistentChange change) {
-        // TODO
-        return false;
+        Criteria criteria = getSession()
+                .createCriteria(PersistentChange.class)
+                .add(Restrictions.eq("hash", change.getHash()));
+        Integer count = (Integer) criteria.setProjection(Projections.rowCount()).uniqueResult();
+        return count > 0;
     }
 }
