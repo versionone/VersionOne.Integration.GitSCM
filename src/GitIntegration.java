fe7f982e5f31d7d2f770868d11859be2e19cfb58
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-11-15 16:41:03 +0000

    an example of running periodically

--- src/GitIntegration.java
+++ src/GitIntegration.java
@@ -1,9 +1,30 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Main Class for integration with Git
  */
 public class GitIntegration {
 
-    public static void main(String[] arg) {
+    private final static Timer timer = new Timer();
 
+    public static void main(String[] arg) {
+        // Timer runs all tasks in a single background thread, so its behaviour is similar to .NET events
+        timer.scheduleAtFixedRate(new GitPollTask(), 0, 3000);
+
+        while(true) { /* do nothing, the job is done in background thread */ }
+    }
+
+    private static class GitPollTask extends TimerTask {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(4000);
+                System.out.println("hit next interval");
+            } catch(InterruptedException ex) {
+                // do nothing
+            }
+        }
     }
 }
