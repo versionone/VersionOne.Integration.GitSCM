ae0bcdc087c212e222174a067734d7504c2a9664
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-11-16 10:06:50 +0000

    added stub for tests and coverage to build script

---
+++ Tests/com/versionone/git/TestStub.java
@@ -0,0 +1,21 @@
+package com.versionone.git;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Solomin
+ * Date: 16.11.2010
+ * Time: 12:50:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestStub {
+
+    @Test
+    public void test() {
+        Assert.assertTrue("Testing works", true);
+    }
+    
+}
