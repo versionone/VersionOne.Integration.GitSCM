03daf410ca87c19fd351b6093d31d4b8409b2072
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-11-18 12:30:37 +0000

    added log4j support changed build structure xml files moved to resources

--- src/com/versionone/git/Main.java
+++ src/com/versionone/git/Main.java
@@ -1,15 +1,20 @@
 package com.versionone.git;
 
+import org.apache.log4j.Logger;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
 
     private final static Timer timer = new Timer();
+    private static final Logger LOG = Logger.getLogger("GetIntegration");
 
     public static void main(String[] arg) {
         // TODO parse configuration
+        LOG.info("Loading config..");
         Configuration configuration = new Configuration();
+        LOG.info("Configuration loaded..");
 
         timer.scheduleAtFixedRate(new GitPollTask(configuration), 0, configuration.getTimeoutMillis());
 
@@ -21,12 +26,16 @@ public class Main {
         private final GitService service;
 
         GitPollTask(Configuration configuration) {
+            LOG.info("Creating service...");
             service = new GitService(configuration);
+            LOG.info("Service created.");
         }
 
         @Override
         public void run() {
+            LOG.info("Running processing..");
             service.onInterval();
+            LOG.info("Processing completed.");
         }
     }
 }
